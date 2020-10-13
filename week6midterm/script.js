c1d67c81864d7a50dfccc16ea6f8d87163cf24b3
brian leisner <leisner_brian_s@student.smc.edu>
2020-10-12 22:33:23 -0700

    part one midterm
    things to change upcoming

--- week6midterm/script.js
+++ week6midterm/script.js
@@ -1,20 +1,23 @@
 $('#myModal').modal('show');
-$('#joinNow').click(function(){
+
+$('#joinNow').click(function () {
     $('#myModal').modal('hide');
     alert('Thank you for joining us.');
-
 })
 
-$(document).ready(function(){
-    $('.carousel').carousel({interval : 1400, keyboard : false});
+$(document).ready(function () {
+    $('.carousel').carousel({
+        interval: 2000,
+        keyboard: false
+    });
 })
+
 let carouselPaused = false;
-$('.btn-primary').click(function(){
-    if(carouselPaused){
+$('.btn-primary').click(function () {
+    if (carouselPaused) {
         $('.carousel').carousel('cycle');
         $(this).text("Pause");
-    }
-    else {
+    } else {
         $('.carousel').carousel('pause');
         $(this).text("Play");
     }
