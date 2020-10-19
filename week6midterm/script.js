155d28f281818b1deb4a1a97a51f7187a8954795
brian leisner <leisner_brian_s@student.smc.edu>
2020-10-19 02:12:58 -0700

    closer to midterm
    midterm near

--- week6midterm/script.js
+++ week6midterm/script.js
@@ -1,19 +1,38 @@
 $('#myModal').modal('show');
 
-$('#joinNow').click(function () {
-    $('#myModal').modal('hide');
-    alert('Thank you for joining us.');
-})
+
+ let form= document.getElementById('myForm');
+ form.addEventListener("submit", function(evt){
+     if(form.checkValidity()===false){
+ evt.preventDefault();
+ evt.stopPropagation();
+     }
+    
+    else {(form.parentNode=== myModal) 
+        evt.preventDefault();
+        evt.stopPropagation();
+
+        $('#myModal').modal('hide');
+        alert('Thank you for your submission.');
+    }
+form.classList.add('was-validated');
+ });
+
+
+
+ 
+
+    
 
 $(document).ready(function () {
     $('.carousel').carousel({
         interval: 2000,
         keyboard: false
     });
-})
+});
 
 let carouselPaused = false;
-$('.btn-primary').click(function () {
+$('.primary').click(function () {
     if (carouselPaused) {
         $('.carousel').carousel('cycle');
         $(this).text("Pause");
