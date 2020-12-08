45bd2ea632c9aed78e63d2348690094dbb4e7c14
brian leisner <leisner_brian_s@student.smc.edu>
2020-12-07 23:57:48 -0800

    tyring
    triing

--- weekbrief/script.js
+++ weekbrief/script.js
@@ -1,11 +1,32 @@
-var itemNum=0;
-var button=document.getElementById('btn-now');
-var display=document.getElementById('itemNum');
-button.addEventListener('click',function(){
- 
-    itemNum +=1;
-    document.getElementById('itemNum').innerHTML=itemNum;
 
+
+
+$(document).ready(function(){
+    $('.carousel').carousel({
+        interval: 5000,
+        keyboard: false
+    });
 });
+
+let carouselPaused = false;
+$('.playbutton').click(function(){
+    if (carouselPaused) {
+        $('.carousel').carousel('cycle');
+        $(this).text("Pause");
+    } else {
+        $('.carousel').carousel('pause');
+        $(this).text("Play");
+    }
+    carouselPaused = !carouselPaused;
+});
+
+var itemNum=0;
+ var button1=document.getElementById('btn-now');
+ var display=document.getElementById('itemNum');
+ button1.addEventListener('click',function(){
+    
+     itemNum +=1;
+     document.getElementById('itemNum').innerHTML=itemNum;
+ });
 
 document.querySelector("#myCard").classList.toggle("flip");
